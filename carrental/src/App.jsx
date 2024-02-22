403f9078e646740666622d4a8e640b9cfcd3bdc9
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-22 17:51:10 +0500

    REFACTORING

--- carrental/src/App.jsx
+++ carrental/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-gray ">
+    <h1 className="text-3xl font-bold underline  ">
       Hello world! 
     </h1>
   )
