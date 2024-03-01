d2ac461138c56f061ee82f1166fbeb53da736d20
sameerbutt28 <sameerofficial929@gmail.com>
2024-03-01 23:00:43 +0500

    REFACTORING

--- carrental/src/App.jsx
+++ carrental/src/App.jsx
@@ -1,7 +1,8 @@
+import Navbar from "./components/Navbar"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline  ">
-      Hello world! 
-    </h1>
+   <div>
+    <Navbar/>
+   </div>
   )
 }
