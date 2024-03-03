5c035334fc77d67efe319f57092db39a46ded590
sameerbutt28 <sameerofficial929@gmail.com>
2024-03-03 23:07:53 +0500

    ADDING SOME MORE CODE

--- carrental/src/components/Landing.jsx
+++ carrental/src/components/Landing.jsx
@@ -3,8 +3,20 @@ const Landing = () => {
   return (
     <div className='flex flex-row justify-around'>
       
-<div className=''>
-<h2>item</h2>
+<div className='flex flex-col mt-10'>
+<p className=" font-bold text-xl">Plan your trip now</p>
+<div className="flex flex-row">
+    <p className="text-7xl font-extrabold">Save &nbsp;  </p>
+<p className="text-7xl font-extrabold text-yellow-500"> big </p>
+<p className="text-7xl font-extrabold">&nbsp;  with our </p>
+</div>
+<p className="text-7xl font-extrabold">  car rental</p>
+<p className="mt-3 text-gray-700">Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible </p>
+<p className=" text-gray-700"> pick-up options and much more.</p>
+<button className="m-2 font-bold bg-yellow-500 p-4 border-black">
+        <a href="#">Book Ride Now</a>
+          
+        </button>
 </div>
 <div className=''>
 <img src={Bigcar} alt="landingcar" />
