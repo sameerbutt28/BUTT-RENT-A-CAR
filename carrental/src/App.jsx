3e0b95465f18a93a57785817bbb162fe94cb2239
sameerbutt28 <sameerofficial929@gmail.com>
2024-03-15 02:44:17 +0500

    REFACTORING

--- carrental/src/App.jsx
+++ carrental/src/App.jsx
@@ -12,6 +12,7 @@ export default function App() {
       </div>
       <div className=" mt-10 mb-10 " >
         <Hero />
+        
       </div>
       <div className=" mt-10 mb-10 " >
         <Booking />
