9ecf2470c2b8170050a1d507d7a73c171563b92a
sameerbutt28 <sameerofficial929@gmail.com>
2024-03-15 02:42:24 +0500

    REFACTORING

--- carrental/src/App.jsx
+++ carrental/src/App.jsx
@@ -1,19 +1,28 @@
-import Booking from "./components/Booking"
-import Collection from "./components/Collection"
-import Faq from "./components/Faq"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import Vehicles from "./components/Vehicles"
+import Booking from "./components/Booking";
+import Collection from "./components/Collection";
+import Faq from "./components/Faq";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Vehicles from "./components/Vehicles";
 export default function App() {
   return (
-   <div className="max-conatiner">
-    <Navbar/>
-    <Hero/>  
-    <Booking/>
-    {/* <Collection/> */}
-    <Faq/>
-
-  <Vehicles/>
-   </div>
-  )
+    <div className="max-conatiner">
+      <div className="  " >
+        <Navbar />
+      </div>
+      <div className=" mt-10 mb-10 " >
+        <Hero />
+      </div>
+      <div className=" mt-10 mb-10 " >
+        <Booking />
+      </div>
+      {/* <Collection/> */}
+      <div className=" mt-10 mb-10 " >
+        <Faq />
+      </div>
+      <div className=" mt-10 mb-10 " >
+        <Vehicles />
+      </div>
+    </div>
+  );
 }
--- carrental/src/components/Vehicles.jsx
+++ carrental/src/components/Vehicles.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Vehicles = () => {
   return (
-    <div>
-       <div className="flex flex-col items-center justify-center m-6 p-2">
-        <p className="font-bold text-xl text-center p-2">Plan your trip now</p>
+    <div >
+      <div className="flex flex-col items-center justify-center m-6 p-2">
+        <p className="font-bold text-xl text-center p-2">Vehicle Models</p>
         <p className="text-6xl font-extrabold text-center p-2">
-          Quick & easy car rental
+          Our rental fleet{" "}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Vehicles
+export default Vehicles;
