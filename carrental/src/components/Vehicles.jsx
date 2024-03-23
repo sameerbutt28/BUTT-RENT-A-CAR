1e67d7cb080260871f17b0864df4427acdfe5b1f
sameerbutt28 <sameerofficial929@gmail.com>
2024-03-24 03:01:25 +0500

    UPDATING VEHICLE.JSX

--- carrental/src/components/Vehicles.jsx
+++ carrental/src/components/Vehicles.jsx
@@ -2,67 +2,79 @@ import { useState } from "react";
 
 const Vehicles = () => {
   const [data, setData] = useState({
-    model: "rs",
-    mark:"honda",    
+    model: "Gli",
+    mark:"Grande",    
     year:"2019",  
     doors:"2",
-    AC: "yes",   
-    fuel:"petrol",
+    AC: "Yes",   
+    fuel:"Petrol",
     rent:"$35"
   });
-  const handlehonda = () => {
+  const handlegrande = () => {
     setData({
-      model: "rs",
-      mark:"honda",
-      year:"2019",
+      model: "Gli",
+      mark:"Grande",    
+      year:"2019",  
       doors:"2",
-      AC: "yes",
-      fuel:"petrol",
+      AC: "Yes",   
+      fuel:"Petrol",
       rent:"$35"
     });
   };
   const handecivic = () => {
     setData({
-      model: "turbo",
-      mark:"civic",
+      model: "Turbo",
+      mark:"Civic",
       year:"2018",
       doors:"4",
-      AC: "yes",
-      fuel:"petrol",
+      AC: "Yes",
+      fuel:"Diesel",
       rent:"$24"
     });
   };
   const handleaudi = () => {
     setData({
-      model: "gt",
-      mark:"audi",
+      model: "Etron-gt",
+      mark:"Audi",
       year:"2023",
       doors:"2",
-      AC: "no",
-      fuel:"electric",
+      AC: "Yes",
+      fuel:"Electric",
       rent:"$50"
     });
   };
   const handlegtr= () => {
     setData({
-      model: "g-28",
-      mark:"gtr",
+      model: "G-28",
+      mark:"Gtr",
       year:"2015",
       doors:"4",
-      AC: "yes",
-      fuel:"petrol",
+      AC: "Yes",
+      fuel:"Petrol",
       rent:"$15"
     });
   };
+  const handlelexux= () => {
+    setData({
+      model: "570",
+      mark:"Lexus",
+      year:"2017",
+      doors:"5",
+      AC: "Yes",
+      fuel:"Petrol",
+      rent:"$65"
+    });
+  };
+ 
  
   return (
     <div>
       <div className="flex flex-col items-center justify-center m-6 p-2">
-        <p className="font-bold text-xl text-center p-2">Vehicle Models</p>
-        <p className="text-5xl font-extrabold text-center p-2">
+        <p className="font-bold text-2xl text-center p-2">Vehicle Models</p>
+        <p className="text-3xl font-extrabold text-center p-2">
           Our rental fleet{" "}
         </p>
-        <p className="text-gray-600 p-3">
+        <p className="text-gray-600 text-lg p-3">
           Choose from a variety of our amazing vehicles to rent for your next
           adventure or business trip
         </p>
@@ -71,69 +83,77 @@ const Vehicles = () => {
       <div className="flex flex-row  justify-evenly ">
         <div className="flex flex-col">
           <button
-            onClick={handlehonda}
-            className="m-2 font-bold bg-yellow-500 p-4 border-black "
+            onClick={handlegrande}
+            className="m-2 font-bold bg-gray-200 hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 active:text-black text-black p-6 border-black"
           >
-            {" "}
-            honda
+            Grande
           </button>
           <button
             onClick={handecivic}
-            className="m-2 font-bold bg-yellow-500 p-4 border-black "
+            className="m-2 font-bold bg-gray-200 hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 active:text-black text-black p-6 border-black"
           >
-            {" "}
-            civic
+            Civic
           </button>
-          <button             onClick={handleaudi}
- className="m-2 font-bold bg-yellow-500 p-4 border-black">
-            {" "}
-            audi
+          <button
+            onClick={handleaudi}
+            className="m-2 font-bold bg-gray-200 hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 active:text-black text-black p-6 border-black"
+          >
+            Audi
           </button>
-          <button             onClick={handlegtr}
- className="m-2 font-bold bg-yellow-500 p-4 border-black">
-            {" "}
-            gtr
+          <button
+            onClick={handlegtr}
+            className="m-2 font-bold bg-gray-200 hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 active:text-black text-black p-6 border-black"
+          >
+            GTR
+          </button>
+          <button
+            onClick={handlelexux}
+            className="m-2 font-bold bg-gray-200 hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 active:text-black text-black p-6 border-black"
+          >
+            Lexux 570
           </button>
         </div>
+
         <div className="flex justify-center items-center">
           <img src="" alt="car-img" />
         </div>
-        <div>
-        <table border="1">
-  <thead>
-    <tr>
-      <th>{data.rent}</th>
-      <th>/ rent per day</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Model</td>
-      <td>{data.model}</td>
-    </tr>
-    <tr>
-      <td>Mark</td>
-      <td>{data.mark}</td>
-    </tr>
-    <tr>
-      <td>Year</td>
-      <td>{data.year}</td>
-    </tr>
-    <tr>
-      <td>Doors</td>
-      <td>{data.doors}</td>
-    </tr>
-    <tr>
-      <td>AC</td>
-      <td>{data.AC}</td>
-    </tr>
-    <tr>
-      <td>Fuel</td>
-      <td>{data.fuel}</td>
-    </tr>
-  </tbody>
-</table>
+        <div className="p-6 rounded-lg ">
+          <table className="w-full border-collapse border border-gray-900 text-lg items-center">
+            <thead className="bg-yellow-500 text-black">
+              <tr>
+                <th className="py-4 px-6 text-center  font-bold text-gray-800">{data.rent}</th>
+                <th className="py-4 px-6 text-center  font-bold">/ rent per day</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="py-4 px-6 text-center border font-bold">Model</td>
+                <td className="py-4 px-6 text-center border font-bold text-gray-500">{data.model}</td>
+              </tr>
+              <tr className="text-center">
+                <td className="py-4 px-6 border font-bold ">Mark</td>
+                <td className="py-4 px-6 border font-bold text-gray-500">{data.mark}</td>
+              </tr>
+              <tr className="text-center">
+                <td className="py-4 px-6 border font-bold">Year</td>
+                <td className="py-4 px-6 border font-bold text-gray-500">{data.year}</td>
+              </tr>
+              <tr className="text-center">
+                <td className="py-4 px-6 border font-bold">Doors</td>
+                <td className="py-4 px-6 border font-bold text-gray-500">{data.doors}</td>
+              </tr>
+              <tr className="text-center">
+                <td className="py-4 px-6 border font-bold">AC</td>
+                <td className="py-4 px-6 border font-bold text-gray-500">{data.AC}</td>
+              </tr>
+              <tr className="text-center">
+                <td className="py-4 px-6 border font-bold">Fuel</td>
+                <td className="py-4 px-6 border font-bold text-gray-500">{data.fuel}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+
       </div>
     </div>
   );
