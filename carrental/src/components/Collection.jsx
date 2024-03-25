4106226a9ce7e344c57e681386ccdac9e4b45e4b
sameerbutt28 <sameerofficial929@gmail.com>
2024-03-26 03:34:33 +0500

    GETDOCS

--- carrental/src/components/Collection.jsx
+++ carrental/src/components/Collection.jsx
@@ -1,4 +1,4 @@
-import { doc, setDoc, collection, getDocs } from "firebase/firestore";
+import { doc, setDoc, getDocs } from "firebase/firestore";
 import { firestore } from "./firebase";
 import { useState } from "react";
 
